bf56c7a7a180a935d871d571703cca9b27f6d863
Hira-Zaheer <hirazaheer842@gmail.com>
2023-09-10 13:32:25 +0500

    initial

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image , ImageBackground } from 'react-native'
+import React from 'react'
+const img = require('./assets/adaptive-icon.png')
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    {/* <Image style={{width:300,height:300}} source={require('./assets/adaptive-icon.png')}/>
+    <Image source={{uri: 'https://picsum.photos/200'}} style={{width:300,height:300}}/> */}
+    <ImageBackground source={img} style={{flex:1}}>
+   
+    </ImageBackground>
     </View>
-  );
+  )
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container : {
+    flex:1,
+    backgroundColor:'plum',
+    padding:20,
+  }
+})
